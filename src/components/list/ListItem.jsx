4569bd501d087eb9ce5c8673d50388eb04900f51
Lemos <37798301+Lemos467@users.noreply.github.com>
2022-02-25 00:46:28 +0000

    fix

--- src/components/list/ListItem.jsx
+++ src/components/list/ListItem.jsx
@@ -4,8 +4,7 @@ import { useState } from "react";
 
 const ListItem = ({ index, title, overview, release_date, vote_average, poster_path, name, backdrop_path,first_air_date }) => {
   const [isHovered, setIsHovered] = useState(false);
-  // const trailer = "https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761";
-
+  
   const rateColor = (rate) => {
     if(rate>=8.5){
       return "excelent";
